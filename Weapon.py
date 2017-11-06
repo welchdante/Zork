6919c6613c576fd2aa5bd675518ed2478c236c91
Alex <fountaia@mail.gvsu.edu>
2017-11-06 11:02:59 -0500

    Fixed ifs

--- Weapon.py
+++ Weapon.py
@@ -6,19 +6,19 @@ class Weapon:
 		if(other = true)
 		
 			r = random.randint(0,2)
-			if(r = 0)
+			if r == 0 :
 				self.weapon_name = "Sour Straws"
 				self.attack = random.uniform(1,1.75)
 				self.use_value = 2
 				return self
 			
-			if(r = 1)
+			if r == 1 :
 				self.weapon_name = "Chocolate Bars"
 				self.attack = random.uniform(2,2.4)
 				self.use_value = 4
 				return self
 		
-			if(r = 2)
+			if r == 2 :
 				self.weapon_name = "Nerd Bombs"
 				self.attack = random.uniform(3.5,5)
 				self.use_value = 1
