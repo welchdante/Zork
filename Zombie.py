55b660c32beb0bba326d3a5a93b4c44e1952abc1
Dante Welch <welchdan@mail.gvsu.edu>
2017-11-07 19:34:37 -0500

    Revert "Af player"

--- Zombie.py
+++ Zombie.py
@@ -1,4 +1,7 @@
 from Monster import *
 from random import randint
 class Zombie(Monster):
-	import random
+
+      def __init__(self):
+            self.health = randint(50,100)
+            self.attack = 0
