cb92e64d5530603e77dfc62aace54892cc292ee0
Dante Welch <welchdan@mail.gvsu.edu>
2017-11-09 08:54:07 -0500

    one more for the one time

--- Weapon.py
+++ Weapon.py
@@ -1,23 +1,23 @@
-import random
+from random import random
 class Weapon:
 
      def __init__(self, other = true):
 		if other:
 		
-			r = random.randint(0,2)
+			r = randint(0,2)
 			if r == 0 :
 				self.weapon_name = "Sour Straws"
-				self.attack = random.uniform(1,1.75)
+				self.attack = uniform(1,1.75)
 				self.use_value = 2
 			
 			if r == 1 :
 				self.weapon_name = "Chocolate Bars"
-				self.attack = random.uniform(2,2.4)
+				self.attack = uniform(2,2.4)
 				self.use_value = 4
 		
 			if r == 2 :
 				self.weapon_name = "Nerd Bombs"
-				self.attack = random.uniform(3.5,5)
+				self.attack = uniform(3.5,5)
 				self.use_value = 1
 		
 		else:
