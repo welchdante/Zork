80eebd4cbf5d8babdb46cdd5d74ffc7ff4ad001d
Dante Welch <welchdan@mail.gvsu.edu>
2017-11-12 21:03:35 -0500

    some stuff for the game state

--- Game.py
+++ Game.py
@@ -4,14 +4,21 @@ from Player import *
 class Game:
 
     def __init__(self):
-        self.x = 0
+        self.game_won = False
+
 
     def init_board(self):
-        neighborhood = Neighborhood(4, 4)
+        neighborhood = Neighborhood(2, 2)
         grid = neighborhood.make_grid(neighborhood.height, neighborhood.width)
         grid = neighborhood.fill_neighborhood(grid)
         pprint(grid)
 
+    #def get_game_state():
+
+
 game = Game()
 game.init_board()  
-#player = Player()      
+player = Player()      
+
+
+
