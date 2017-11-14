82b6590a39f24a64b8f8ba610c24cd25a740a2e1
Dante Welch <welchdan@mail.gvsu.edu>
2017-11-13 21:46:02 -0500

    Player can move around

--- Game.py
+++ Game.py
@@ -9,7 +9,7 @@ class Game:
         self.game_won = False
 
     def init_board(self):
-        self.neighborhood = Neighborhood(2, 2)
+        self.neighborhood = Neighborhood(4, 4)
         self.grid = self.neighborhood.make_grid(self.neighborhood.height, self.neighborhood.width)
         self.grid = self.neighborhood.fill_neighborhood(self.grid)
         #pprint(self.grid)
@@ -17,16 +17,16 @@ class Game:
     def spawn_player(self, player, game):
         rand_width = randint(0, game.neighborhood.width - 1)
         rand_height = randint(0, game.neighborhood.height - 1)
-        pprint("Contents in the home")
+        #pprint("Contents in the home")
         game.grid[rand_width][rand_height].append(player)
-        pprint(game.grid[rand_width][rand_height])
+        self.current_width = rand_width
+        self.current_height = rand_height
+        #pprint(game.grid[rand_width][rand_height])
 
     def get_user_move(self, player, game):
-        
         possible_moves = {'w': 'North', 's': 'South', 'd': 'East', 'a': 'West', 
                             'W': 'North', 'S': 'South', 'D': 'East', 'A': 'West'}
         move = input("Which way?")
-        print(move)
         if move in possible_moves:
             the_move = possible_moves[move]
             self.movement(player, game, the_move)
@@ -35,16 +35,59 @@ class Game:
 
     def movement(self, player, game, the_move):
         if the_move == 'North':
-            print("Moving north")
+            self.move_north(player, game, self.current_width, self.current_height)
         elif the_move == 'South':
-            print("Moving south")
+            self.move_south(player, game, self.current_width, self.current_height)
         elif the_move == 'East':
-            print("Moving east")
+            self.move_east(player, game, self.current_width, self.current_height)
         elif the_move == 'West':
-            print("Moving west")
+            self.move_west(player, game, self.current_width, self.current_height)
         else:
             print("Invalid move")
 
+    def move_north(self, player, game, current_width, current_height):
+        print("Move north")
+        if current_height - 1 >= 0:
+            game.grid[current_width][current_height].pop(len(game.grid[current_width][current_height]) - 1)
+            current_height = current_height - 1
+            game.grid[current_width][current_height].append(player)
+            self.current_width = current_width
+            self.current_height = current_height
+        else: 
+            print("Stay in the neighborhood to save the world!")
+
+    def move_south(self, player, game, current_width, current_height):
+        print("Move south")
+        if current_height + 1 < self.neighborhood.height:
+            game.grid[current_width][current_height].pop(len(game.grid[current_width][current_height]) - 1)
+            current_height = current_height + 1
+            game.grid[current_width][current_height].append(player)
+            self.current_width = current_width
+            self.current_height = current_height
+        else: 
+            print("Stay in the neighborhood to save the world!")
+    
+    def move_east(self, player, game, current_width, current_height):
+        print("Move east")
+        if current_width + 1 < self.neighborhood.width:
+            game.grid[current_width][current_height].pop(len(game.grid[current_width][current_height]) - 1)
+            current_width = current_width + 1
+            game.grid[current_width][current_height].append(player)
+            self.current_width = current_width
+            self.current_height = current_height
+        else: 
+            print("Stay in the neighborhood to save the world!")
+    
+    def move_west(self, player, game, current_width, current_height):
+        print("Move west")
+        if current_width - 1 >= 0:
+            game.grid[current_width][current_height].pop(len(game.grid[current_width][current_height]) - 1)
+            current_width = current_width - 1
+            game.grid[current_width][current_height].append(player)
+            self.current_width = current_width
+            self.current_height = current_height
+        else: 
+            print("Stay in the neighborhood to save the world!")
     #def get_current_tile(self, player, game):
     #    pprint(game.grid[current_width][current_height])
 
@@ -54,4 +97,9 @@ player = Player()
 game.spawn_player(player, game)
 game.get_user_move(player, game)
 
+
+
+
+#print(game.grid[current_width][current_height])
+#print(len(game.grid[current_width][current_height]))
 
